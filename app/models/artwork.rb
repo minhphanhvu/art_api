c4565f6c99c16161022117c1e320be3fb404cb3f
minhphanhvu <minhphanh.vu@gmail.com>
2021-06-13 19:26:15 -0500

    Commenting on models

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,8 +1,9 @@
 class Artwork < ApplicationRecord
   validates_presence_of :title, :image_url, :artist_id
+  belongs_to :user, :foreign_key => 'artist_id'
+  # ArtworkShare many-to-many relationship
   validates :title, uniqueness: { scope: :artist_id,
     message: 'Each artist must have unique titles(titles can be the same for different artists)' }
-  belongs_to :user, :foreign_key => 'artist_id'
   has_many :artwork_shares, :foreign_key => 'artwork_id'
   has_many :shared_viewers, through: :artwork_shares, :foreign_key => 'artwork_id', :class_name => 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :artworks, :foreign_key => 'artist_id', dependent: :destroy
+  # Artworkshare many-to-many relationship
   has_many :artwork_shares, :foreign_key => 'viewer_id', dependent: :destroy
   has_many :shared_artworks, through: :artwork_shares, :foreign_key => 'viewer_id', :class_name => 'Artwork'
 end
